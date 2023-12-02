119a63dce15e958416f6f42fe874d0d3d1bd3ed4
Michael <michaelbozhilov@gmail.com>
2023-12-02 13:58:29 +0200

    Add button animation

--- src/app/_components/HomeControls/ControlButton.tsx
+++ src/app/_components/HomeControls/ControlButton.tsx
@@ -1,13 +1,49 @@
-"use client"
+"use client";
 
 import { cn } from "@/lib/utils";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-export default function ControlButton({icon, href, className}: {icon: React.ReactNode, href: string, className: string}) {
-    const router = useRouter();
+type FocusedState = "initialized" | "focused" | "unfocused"
+
+const getControlButtonAnimationClassName = (state: FocusedState) => {
+    switch(state) {
+        case "focused": return `animate-button-up`;
+        case "initialized": return ``;
+        case "unfocused": return "animate-button-down"
+    }
+}
+
+export default function ControlButton({
+  icon,
+  href,
+  className,
+}: {
+  icon: React.ReactNode;
+  href: string;
+  className: string;
+}) {
+  const router = useRouter();
+  const [isFocused, setIsFocused] = useState<FocusedState>("initialized");
+
   return (
-    <div className={cn(className, "rounded-3xl m-2 p-4")} onClick={() => {router.push(href)}}>
-        {icon}
+    <div
+      className={cn(
+        className,
+        `m-2 rounded-3xl p-4`,
+        getControlButtonAnimationClassName(isFocused)
+      )}
+      onClick={() => {
+        router.push(href);
+      }}
+      onMouseEnter={() => {
+        setIsFocused("focused");
+      }}
+      onMouseLeave={() => {
+        setIsFocused("unfocused");
+      }}
+    >
+      {icon}
     </div>
   );
 }
