f11a244bcc6d1e8b1a623f9e73382d17641bfbf5
Michael <michaelbozhilov@gmail.com>
2023-12-06 23:53:56 +0200

    Add lint-staged and husky

--- src/app/dashboard/courses/page.tsx
+++ src/app/dashboard/courses/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { type Course } from "@prisma/client";
 import { useState } from "react";
@@ -8,28 +8,33 @@ import { api } from "~/trpc/react";
 export default function Courses() {
   const [filter, setFilter] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const {data} = api.course.getFilteredCourses.useQuery({filter, page: currentPage});
+  const { data } = api.course.getFilteredCourses.useQuery({
+    filter,
+    page: currentPage,
+  });
 
   return (
     <div>
       <SearchBar setFilter={setFilter} />
-      <RenderCourses courses={data}/>
-      <div onClick={() => setCurrentPage(state => state + 1)}>Next</div>
+      <RenderCourses courses={data} />
+      <div onClick={() => setCurrentPage((state) => state + 1)}>Next</div>
     </div>
   );
 }
 
-function RenderCourses({courses}: {courses?: Course[]}) {
+function RenderCourses({ courses }: { courses?: Course[] }) {
   return (
     <div className="w-full max-w-xs">
-      {courses
-        ? courses.map((course) => (
-            <div key={course.id}>
-              <p>{course.name}</p>
-              <p>{course.description}</p>
-            </div>
-          ))
-        : <h1>LOADING</h1>}
+      {courses ? (
+        courses.map((course) => (
+          <div key={course.id}>
+            <p>{course.name}</p>
+            <p>{course.description}</p>
+          </div>
+        ))
+      ) : (
+        <h1>LOADING</h1>
+      )}
     </div>
   );
 }
