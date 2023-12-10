2f4a5208f4d471775973d243b7663fe0c18596a2
Michael <michaelbozhilov@gmail.com>
2023-12-10 20:57:44 +0200

    Add logic for seeding electives

--- prisma/utils/seeding/seed.ts
+++ prisma/utils/seeding/seed.ts
@@ -2,8 +2,12 @@ import { PrismaClient } from "@prisma/client";
 import { seedOrganization } from "./seedOrganization";
 import aubgOrganization from "./seedingData/aubg/organization.json";
 import xyzOrganization from "./seedingData/xyzuni/organization.json";
+import { seedProgram } from "./seedProgram";
+import aubgBusinessAdministration from "./seedingData/aubg/programs/business_administration.json";
 
 const prisma = new PrismaClient();
 
 await seedOrganization(prisma, aubgOrganization);
 await seedOrganization(prisma, xyzOrganization);
+
+await seedProgram(prisma, aubgBusinessAdministration, "aubg");
--- prisma/utils/seeding/seedProgram.ts
+++ prisma/utils/seeding/seedProgram.ts
@@ -1,2 +1,114 @@
 // TODO: seedProgram.ts should export a function which will seed a program
 // e.g seedProgram(prisma, aubgBusAdministration, "aubg")
+
+import {
+  Course,
+  Program,
+  type PrismaClient,
+  ElectiveGroup,
+} from "@prisma/client";
+import type program from "./seedingData/aubg/programs/business_administration.json";
+import { connect } from "http2";
+
+export const seedProgram = async (
+  prisma: PrismaClient,
+  prog: typeof program,
+  org: "aubg" | "xyzuni",
+) => {
+  try {
+    const program = await prisma.program.upsert({
+      where: {
+        name_organizationId: {
+          name: prog.name,
+          organizationId: org,
+        },
+      },
+      update: {},
+      create: {
+        name: prog.name,
+        requiredCredits: 120,
+        Organization: {
+          connect: {
+            id: org,
+          },
+        },
+      },
+    });
+
+    console.log({ program });
+
+    const foundationalCourses = await prisma.program.update({
+      where: {
+        name_organizationId: {
+          name: prog.name,
+          organizationId: org,
+        },
+      },
+      data: {
+        foundationalCourses: {
+          connect: prog.foundationalCourses.map((course) => ({
+            codeName_organizationId: {
+              codeName: course.codeName,
+              organizationId: org,
+            },
+          })),
+        },
+      },
+    });
+
+    const electiveGroupsPromises: Promise<ElectiveGroup>[] = [];
+
+    prog.electiveGroups.forEach((group) => {
+      electiveGroupsPromises.push(
+        prisma.electiveGroup.upsert({
+          where: {
+            name_programId: {
+              name: group.name,
+              programId: program.id,
+            },
+          },
+          update: {},
+          create: {
+            name: group.name,
+            requiredCredits: group.requiredCredits,
+            ...(group.requiredCourses
+              ? {
+                  requiredCourses: {
+                    createMany: {
+                      data: group.requiredCourses.map((course) => ({
+                        codeName: course.codeName,
+                        organizationId: org,
+                      })),
+                    },
+                  },
+                }
+              : {}),
+            ...(group.electiveCourses
+              ? {
+                  electiveCourses: {
+                    createMany: {
+                      data: group.electiveCourses.map((course) => ({
+                        codeName: course.codeName,
+                        organizationId: org,
+                      })),
+                    },
+                  },
+                }
+              : {}),
+          },
+        }),
+      );
+    });
+
+    const electiveCourses = await Promise.all(electiveGroupsPromises);
+
+    console.log({ electiveCourses });
+  } catch (error) {
+    console.error(error);
+    await prisma.$disconnect().catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+    });
+  }
+};
