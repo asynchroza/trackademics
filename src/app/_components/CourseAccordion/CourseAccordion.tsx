11e0d3b1ee568039f96eb05cdf80075f456a4a9c
Michael <michaelbozhilov@gmail.com>
2024-01-04 01:57:24 +0200

    Add initial version of program rendering accordion

---
+++ src/app/_components/CourseAccordion/CourseAccordion.tsx
@@ -0,0 +1,151 @@
+"use client";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { type Course, type Prisma } from "@prisma/client";
+import {
+  useReactTable,
+  flexRender,
+  getCoreRowModel,
+  createColumnHelper,
+} from "@tanstack/react-table";
+
+type PickedCourse = Pick<Course, "codeName" | "name">;
+
+const columnHelper = createColumnHelper<PickedCourse>();
+
+const columns = [
+  columnHelper.accessor("codeName", {
+    header: () => "Code",
+    cell: (info) => info.getValue(),
+  }),
+  columnHelper.accessor("name", {
+    header: () => "Name",
+    cell: (info) => info.getValue(),
+  }),
+];
+
+function CourseTable({ data }: { data: PickedCourse[] }) {
+  const table = useReactTable({
+    columns,
+    data,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  if (!table.getRowModel().rows?.length) return null;
+
+  return (
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows.map((row) => (
+            <TableRow
+              key={row.id}
+              data-state={row.getIsSelected() && "selected"}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
+
+// https://github.com/prisma/prisma/discussions/10928
+
+type Program = Prisma.ProgramGetPayload<{
+  include: {
+    foundationalCourses: {
+      select: {
+        courses: true;
+      };
+    };
+    electiveGroups: {
+      select: {
+        requiredCourses: {
+          select: {
+            codeName: true;
+            course: true;
+          };
+        };
+        requiredCredits: true;
+        electiveCourses: {
+          select: {
+            codeName: true;
+            course: true;
+          };
+        };
+        rules: true;
+        name: true;
+      };
+    };
+  };
+}>;
+
+export const CourseAccordion = ({ program }: { program: Program }) => {
+  return (
+    <Accordion type="single" collapsible className="w-[40vw]">
+      <AccordionItem value="foundational">
+        <AccordionTrigger>Foundational Courses</AccordionTrigger>
+        <AccordionContent>
+          <CourseTable data={program.foundationalCourses.courses} />
+        </AccordionContent>
+      </AccordionItem>
+      {program.electiveGroups.map((electiveGroup) => (
+        <AccordionItem value={electiveGroup.name} key={electiveGroup.name}>
+          <AccordionTrigger>{electiveGroup.name}</AccordionTrigger>
+          <AccordionContent>
+            <CourseTable
+              data={electiveGroup.requiredCourses.map(
+                (course) => course.course,
+              )}
+            />
+          </AccordionContent>
+
+          <AccordionContent>
+            <CourseTable
+              data={electiveGroup.electiveCourses.map(
+                (course) => course.course,
+              )}
+            />
+          </AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
+  );
+};
--- src/app/dashboard/programs/page.tsx
+++ src/app/dashboard/programs/page.tsx
@@ -1,70 +1,16 @@
+import { CourseAccordion } from "~/app/_components/CourseAccordion/CourseAccordion";
 import { api } from "~/trpc/server";
 
 export default function Programs() {
   return <RenderProgram />;
 }
 
-function ProgramContainer({
-  program,
-}: {
-  program: Awaited<ReturnType<typeof api.program.getPrograms.query>>[0];
-}) {
-  return (
-    <div className="flex flex-col">
-      <h1 className="p-2">
-        {program.name} --- Total required credits: {program.requiredCredits}
-      </h1>
-      <h2 className="p-2">Foundational courses:</h2>
-      <div>
-        {program.foundationalCourses.courses.map((course) => (
-          <div key={course.id} className="flex flex-row">
-            <h3>
-              {course.codeName}: {course.name}
-            </h3>
-          </div>
-        ))}
-      </div>
-      <div>
-        {program.electiveGroups.map((eg) => {
-          return (
-            <div key={eg.name}>
-              <h1 className="p-2">
-                {eg.name} --- Required credits: {eg.requiredCredits}
-              </h1>
-              <div>
-                {eg.requiredCourses.map((course) => (
-                  <div key={course.course.id} className="flex flex-row">
-                    <h3>
-                      {course.course.codeName}: {course.course.name}
-                    </h3>
-                  </div>
-                ))}
-              </div>
-              <div>
-                {eg.electiveCourses.map((course) => (
-                  <div key={course.course.id} className="flex flex-row">
-                    <h3>
-                      {course.course.codeName}: {course.course.name}
-                    </h3>
-                  </div>
-                ))}
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
-}
-
 async function RenderProgram() {
   const programs = await api.program.getPrograms.query();
 
   return (
-    <div className="flex flex-row">
-      {programs.map((program) => (
-        <ProgramContainer key={program.name} program={program} />
-      ))}
+    <div className="flex flex-row justify-center">
+      <CourseAccordion program={programs[0]!} />
     </div>
   );
 }
