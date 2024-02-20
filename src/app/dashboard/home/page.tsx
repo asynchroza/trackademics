1146c91a69fb771955c5b02b624547ad56f099f8
Michael <michaelbozhilov@gmail.com>
2024-02-20 12:21:04 +0200

    Calendar

--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -1,3 +1,28 @@
+"use client";
+import { Scheduler } from "@aldabil/react-scheduler";
+import type {
+  SchedulerHelpers,
+  SchedulerRef,
+} from "@aldabil/react-scheduler/types";
+import { useRef } from "react";
+
+const customEditor = (scheduler: SchedulerHelpers) => {
+  return (
+    <div
+      style={{ width: "200px" }}
+      onClick={() => {
+        scheduler.close();
+      }}
+    >
+      Tuk shte e form-a i drugite neshta
+    </div>
+  );
+};
+
 export default function Home() {
-  return <h1>Home</h1>;
+  const calendarRef = useRef<SchedulerRef>(null);
+
+  return (
+    <Scheduler ref={calendarRef} events={[]} customEditor={customEditor} />
+  );
 }
